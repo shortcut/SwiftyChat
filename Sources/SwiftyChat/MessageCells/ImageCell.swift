794d48db493f4436234aabfc650cac50a0e3f26c
Nikita <me22anine@yandex.ru>
2021-12-03 15:25:34 +0300

    image cache

--- Sources/SwiftyChat/MessageCells/ImageCell.swift
+++ Sources/SwiftyChat/MessageCells/ImageCell.swift
@@ -46,7 +46,7 @@ internal struct ImageLoadingKindCell: View {
     // MARK: - case Remote Image
     @ViewBuilder private func remoteImage(url: URL) -> some View {
         
-        let path = cachePath(forKey: meal.url.cacheKey)
+        let path =  ImageCache.default.cachePath(forKey: meal.url.cacheKey)
         
         url.cacheKey
         if let image = UIImage.init(contentsOfFile: path) {
